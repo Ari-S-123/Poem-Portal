464bfb62a568990e7faf6671e275301d074f0064
Aritra (Ari) Saharay <80832201+Ari-S-123@users.noreply.github.com>
2024-10-24 23:41:45 -0700

    Added shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
 	import '../app.css';
 	import { Moon, Sun } from 'lucide-svelte';
-	import { theme } from '$lib/stores/theme';
-
-	function toggleTheme() {
-		theme.update((current) => (current === 'dark' ? 'light' : 'dark'));
-	}
+	import { toggleMode, ModeWatcher } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
 
 	let { children } = $props();
 </script>
 
-<div
-	class={`min-h-screen transition-colors duration-300 ${$theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}
->
+<div class="min-h-screen transition-colors duration-300">
+	<ModeWatcher />
 	<nav class="p-4 flex justify-end">
-		<button
-			onclick={toggleTheme}
-			class={`p-2 rounded-full hover:scale-105 hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black`}
-			aria-label="Toggle theme"
+		<Button
+			onclick={toggleMode}
+			variant="ghost"
+			size="icon"
+			class="hover:scale-105 transition-all ease-in-out duration-300"
 		>
-			{#if $theme === 'dark'}
-				<Sun class="w-6 h-6" />
-			{:else}
-				<Moon class="w-6 h-6" />
-			{/if}
-		</button>
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
 	</nav>
 	{@render children()}
 </div>
