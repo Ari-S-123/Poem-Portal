fa2fbc39dc78f6211bad34f7c6582e8882368a18
Aritra (Ari) Saharay <80832201+Ari-S-123@users.noreply.github.com>
2024-10-26 00:37:09 -0700

    #8 - Changed dark mode toggle to dropdown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,34 @@
 <script lang="ts">
 	import '../app.css';
 	import { Moon, Sun } from 'lucide-svelte';
-	import { toggleMode, ModeWatcher } from 'mode-watcher';
+	import { resetMode, setMode, ModeWatcher } from 'mode-watcher';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <div class="min-h-screen transition-colors duration-300">
-	<ModeWatcher />
 	<nav class="p-4 flex justify-end">
-		<Button
-			onclick={toggleMode}
-			variant="ghost"
-			size="icon"
-			class="hover:scale-105 transition-all ease-in-out duration-300"
-		>
-			<Sun
-				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-			/>
-			<Moon
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-			/>
-			<span class="sr-only">Toggle theme</span>
-		</Button>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger asChild let:builder>
+				<Button builders={[builder]} variant="outline" size="icon">
+					<Sun
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Moon
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content align="end">
+				<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
 	</nav>
 	{@render children()}
 </div>
